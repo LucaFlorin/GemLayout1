5bacad5cff3b524a4cbd39a083140dfa61ba8247
LucaFlorin <luca.florin.adrian@gmail.com>
2018-08-31 15:16:09 +0300

    progress2

--- GemLayout.js
+++ GemLayout.js
@@ -11,12 +11,28 @@ $(document).ready(function(){
         $('.sidenav').toggleClass('sidenav_active');        
     });
 
+
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 180) {
+            $(".header").addClass("header_active");
+            $(".b_line").addClass('b_line_active');
+            $(".logo_box").addClass('logo_box_hidden');
+            $(".logo_box").addClass('logo_box_active1');
+            $(".logo_box_active1").addClass('logo_box_active2');
+        } else {
+           $(".header").removeClass("header_active");
+           $(".b_line").removeClass('b_line_active');
+           $(".logo_box").removeClass('logo_box_hidden');
+           $(".logo_box").removeClass('logo_box_active1');
+           $(".logo_box_active1").removeClass('logo_box_active2');
+        }
+    });
+
 });
 
 $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
     if(wScroll > $('.about_us').offset().top){
-        console.log(wScroll);
         $('.aboutus_info').addClass('aboutus_info_active');
     }
 
@@ -24,8 +40,13 @@ $(window).scroll(function(){
         $('.card_parent').each(function(i){
             setTimeout(function(){
                 $('.card_parent').eq(i).addClass('card_parent_active');
+                $('.card_icon').eq(i).addClass('card_iconv2');
             }, 150 * (i + 1));
         });
+    }
+
+    if(wScroll > $('.chose_us').offset().top){
+        $('.choseus_title').addClass('choseus_title_active');
     }
         
 });
